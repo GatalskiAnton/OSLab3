236c12986078bc39fad9c9b9b391bf16bf58215b
Anton <ag629331@gmail.com>
2022-09-25 18:07:22 +0300

    added fixes

--- bashScript.sh
+++ bashScript.sh
@@ -2,7 +2,7 @@
 g++ -c -o Number/build/number.o Number/number.cpp
 ar r Number/build/libNumber.a Number/build/number.o
 #build Vector dynamic library and link Number to Vector 
-g++ -c -o Vector/build/vector.o Vector/vector.cpp -I../Number
+g++ -c -o Vector/build/vector.o Vector/vector.cpp -INumber
 g++ -shared -o Vector/build/libVector.so Vector/build/vector.o
 #link Vector and Number to main
 g++ -o main/main.out main/main.cpp -LNumber/build -LVector/build -lNumber -lVector -IVector -INumber
