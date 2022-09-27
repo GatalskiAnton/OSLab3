a86648c44f742c98a71400417f41e2238ba47f7e
Anton <ag629331@gmail.com>
2022-09-27 11:49:45 +0300

    added fixes

--- bashScript.sh
+++ bashScript.sh
@@ -5,7 +5,7 @@ ar r Number/build/libNumber.a Number/build/number.o
 g++ -c -o Vector/build/vector.o Vector/vector.cpp -INumber -LNumber/build -lNumber
 g++ -shared -o Vector/build/libVector.so Vector/build/vector.o
 #link Vector and Number to main
-g++ -o main/main.out main/main.cpp -LNumber/build -LVector/build -lNumber -lVector -IVector -INumber
+g++ -o main/main main/main.cpp -LNumber/build -LVector/build -lNumber -lVector -IVector -INumber
 cd main
 export LD_LIBRARY_PATH=../Vector/build
-./main.out
+./main
